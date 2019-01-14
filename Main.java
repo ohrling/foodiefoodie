dada5ee2edb3f3c896f3e9b53f648e7f03ce8ab2
Tommy Ohrling <tommy@ohrlings.com>
2019-01-14 20:07:32 +0100

    Anders mat måste ju skrivas ut.

--- Main.java
+++ Main.java
@@ -2,7 +2,8 @@ class Main {
 	public static void main(String[] args) {
 		Mat[] buffe = {
 			new HotDog("Lumi"),
-			new Rotmos("Tommy")
+			new Rotmos("Tommy"),
+			new PulledPork("Anders")
 		};
 		for(Mat m: buffe){
 			System.out.println(m);
